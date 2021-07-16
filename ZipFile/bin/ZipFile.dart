accd511bfc8230dc3e3ca9fd82515c8c39ed5d08
LenouarMiloud <lenouarmiloud@gmail.com>
2021-07-16 12:37:31 +0200

    add the Zip Project

---
+++ ZipFile/bin/ZipFile.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:archive/archive.dart';
+import 'package:path/path.dart';
+
+void main(List<String> arguments) {
+
+}
+//zip function
+void zip(List<String> files,String file){
+  Archive archive = new Archive();
+
+  files.forEach((String path) {
+
+  });
+
+}
+//unzip function
+void unzip(String zip,String path){
+
+}
