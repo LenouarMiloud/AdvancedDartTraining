bbef8a17aeea05f995858f1f062c41806d5a8b30
LenouarMiloud <lenouarmiloud@gmail.com>
2021-07-16 19:29:59 +0200

    modify the code in compression project

--- ZipFile/bin/ZipFile.dart
+++ ZipFile/bin/ZipFile.dart
@@ -1,21 +1,53 @@
-import 'dart:convert';
 import 'dart:io';
+import 'dart:convert';
 import 'package:archive/archive.dart';
-import 'package:path/path.dart';
+import 'package:path/path.dart' as p;
 
-void main(List<String> arguments) {
+main(List<String> arguments) {
+
+  List<String> files = new List<String>();
+  Directory.current.listSync().forEach((FileSystemEntity fse) {
+    if(fse.statSync().type == FileSystemEntityType.FILE) files.add(fse.path);
+  });
+
+  String zipfile = '/F/test.zip';
+
+  zip(files, zipfile);
+
+  //unzip(zipfile, '/F/decompressed');
 
 }
-//zip function
-void zip(List<String> files,String file){
+
+void zip(List<String> files, String file) {
   Archive archive = new Archive();
 
-  files.forEach((String path) {
+  files.forEach((String path){
+    File file = new File(path);
+
+    ArchiveFile archivefile = new ArchiveFile(p.basename(path), file.lengthSync(), file.readAsBytesSync());
+    archive.addFile(archivefile);
 
   });
 
-}
-//unzip function
-void unzip(String zip,String path){
+  ZipEncoder encoder = new ZipEncoder();
+  File f = new File(file);
+  f.writeAsBytesSync(encoder.encode(archive));
 
+  print('Compressed');
+
+}
+
+void unzip(String zip, String path) {
+  File file = new File(zip);
+
+  Archive archive = new ZipDecoder().decodeBytes(file.readAsBytesSync());
+
+  archive.forEach((ArchiveFile archivefile) {
+    File file = new File(path + '/' + archivefile.name);
+    file.createSync(recursive: true);
+    file.writeAsBytesSync(archivefile.content);
+
+  });
+
+  print('Decompressed');
 }
