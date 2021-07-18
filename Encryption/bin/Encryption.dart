530fb5c1a399a638a658bb99c0714873615e199e
LenouarMiloud <lenouarmiloud@gmail.com>
2021-07-18 22:33:10 +0200

    add the main code

--- Encryption/bin/Encryption.dart
+++ Encryption/bin/Encryption.dart
@@ -1,3 +1,21 @@
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:pointycastle/pointycastle.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+
+  //initialise the Encrypting Algorithm
+  Digest digest = new Digest("SHA-256");
+
+  String name = "LENOUAR Miloud";
+
+  Uint8List data = new Uint8List.fromList(utf8.encode(name));
+
+  //Process the data
+  Uint8List hash = digest.process(data);
+
+  print(hash);
+  print(base64.encode(hash));
+
+
 }
