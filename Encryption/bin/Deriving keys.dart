573f794e26db62f5b436895dd4755783f01ae511
LenouarMiloud <lenouarmiloud@gmail.com>
2021-07-20 17:25:10 +0200

    add the main code in Deriving key program

--- Encryption/bin/Deriving keys.dart
+++ Encryption/bin/Deriving keys.dart
@@ -1,13 +1,28 @@
 import 'dart:typed_data';
 import 'dart:convert';
+import 'package:pointycastle/export.dart';
 import 'package:pointycastle/pointycastle.dart';
 
 void main(List<String> args){
     String password = "Hack The Box";
+    var hackString = createUint8listFromString('hacKtheboxnow');
+    //the list of algorithm
+    var pkcs = new KeyDerivator("SHA-1/HMAC/PBKDF2");
+
+    var params = new Pbkdf2Parameters(hackString, 100, 16);
+
+    pkcs.init(params);
+
+    Uint8List key = pkcs.process(createUint8listFromString(password));
+
+    display("Key Value", key);
 }
 
-Uint8List CreateUint8listFromString(String value) => new Uint8List.fromList(utf8.encode(value));
+Uint8List createUint8listFromString(String value) => new Uint8List.fromList(utf8.encode(value));
 
 void display(String title,Uint8List value){
-  
+  print(title);
+  print(value);
+  print(base64.encode(value));
+  print('');
 }
