7096e4e47df3dc2f3d517b38896dc098611e3cf3
LenouarMiloud <lenouarmiloud@gmail.com>
2021-07-27 19:38:13 +0200

    add code in the main program

--- Socket/bin/Socket.dart
+++ Socket/bin/Socket.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+import 'dart:convert';
+
+void main(List<String> arguments) async {
+  var serverSocket = await ServerSocket.bind("127.0.0.1", 3000);
+
+  print('Listening ');
+
+  await for(var socket in serverSocket){
+    
+  }
 }
