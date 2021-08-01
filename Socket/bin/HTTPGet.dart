15b8b60cf6b6d6bfe0da25a22286bd20c842e15c
LenouarMiloud <lenouarmiloud@gmail.com>
2021-08-01 15:55:34 +0200

    add the Uri.parse() to myurl variable

--- Socket/bin/HTTPGet.dart
+++ Socket/bin/HTTPGet.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 
 main(List<String> arguments) async {
-  var myurl = 'http://httpbin.org/';
+  var myurl = Uri.parse('http://httpbin.org/');
   var response = await http.get(myurl);
   //show the response status
   print('Response status ${response.statusCode}');
