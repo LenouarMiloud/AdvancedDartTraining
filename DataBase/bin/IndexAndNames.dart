5ceb71391d1c5fb3d707569130ab27b602f7573f
LenouarMiloud <lenouarmiloud@gmail.com>
2021-08-05 12:18:34 +0200

    Update Queries code

--- DataBase/bin/IndexAndNames.dart
+++ DataBase/bin/IndexAndNames.dart
@@ -1,23 +1,28 @@
 import 'dart:io';
+import 'dart:async';
 import 'package:sqljocky5/constants.dart';
 import 'package:sqljocky5/sqljocky.dart';
 
 main(List<String> arguments) async {
-  //create a Connection Pool
+
+  //Connection Setting
   var pool = new ConnectionSettings(
       host: 'localhost',
       port: 3306,
       user: 'fsociety',
       password: 'myPass',
-      db: 'university'
+      db: 'school'
   );
-
-  //selecting from teachers table
+  //the select query
   MySqlConnection conn;
-  var results = await conn.execute('Select * from teachers');
-  //show the number of rows
-  print('Results ${await results.length} rows');
-  //close the connection
+  var results = await conn.execute('select Name, Topic from teachers');
+  //show the result of query
+  await results.forEach((row) {
+    print('Name: ${row[0]}, Topic: ${row[1]}'); //name or index
+
+
+  });
+  //close connection
   conn.close();
   exit(0);
 }
